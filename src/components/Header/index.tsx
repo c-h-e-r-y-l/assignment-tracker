cb2d031766220bcb552e8df498170569bc14786d
Cheryl <cherylstelp@gmail.com>
2025-02-09 20:20:51 -0800

    changed create button behaviour to be disabled with no text or space only inputs. enabled on text input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,31 @@
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
+import { useState } from "react";
 
 export function Header() {
-  return (
-    <header className={styles.header}>
-      {/* This is simply to show you how to use helper functions */}
-      <h1>{uppercase("bcit")} Assignment Tracker</h1>
-      <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
-        <button>
-          Create <AiOutlinePlusCircle size={20} />
-        </button>
-      </form>
-    </header>
-  );
+    const [inputText, setInputText] = useState('');
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value);
+    };
+
+    const isInputEmpty = !inputText.trim();
+
+    return (
+        <header className={styles.header}>
+            {/* This is simply to show you how to use helper functions */}
+            <h1>{uppercase("bcit")} Assignment Tracker</h1>
+            <form className={styles.newAssignmentForm}>
+                <input 
+                    placeholder="Add a new assignment"
+                    type="text"
+                    value={inputText}
+                    onChange={handleInputChange} />
+                <button disabled={isInputEmpty}>
+                    Create <AiOutlinePlusCircle size={20} />
+                </button>
+            </form>
+        </header>
+    );
 }
